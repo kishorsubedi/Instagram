057bc52ac77e916111855b2de07ecb2f0cef982e
Kishor Subedi <Kishor@Sudarshans-MBP.fios-router.home>
2018-02-26 13:27:04 -0500

    Pictures access given

---
+++ UploadViewController.swift
@@ -0,0 +1,59 @@
+//
+//  UploadViewController.swift
+//  Instagram
+//
+//  Created by Kishor Subedi on 2/26/18.
+//  Copyright © 2018 Kishor Subedi. All rights reserved.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var uploadImageView: UIImageView!
+    @IBOutlet weak var captionField: UITextField!
+    
+    @IBAction func uploadImageFromSource(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(vc, animated: true, completion: nil)
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
